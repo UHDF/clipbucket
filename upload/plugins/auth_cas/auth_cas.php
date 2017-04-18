9cd8fef8586c57b2c8da31dd165d2950435b3ba2
Adrien Ponchelet <adrien.ponchelet@u-picardie.fr>
2017-04-18 11:31:03 +0200

    Modify admin menu item entry due to new label

--- upload/plugins/auth_cas/auth_cas.php
+++ upload/plugins/auth_cas/auth_cas.php
@@ -224,6 +224,6 @@ Version: 1.0
 	 *	Add entries for the plugin in the administration pages
 	 */
 	if ($cbplugin->is_installed('common_library.php') && $userquery->permission[getStoredPluginName("authcas")]=='yes')
-	add_admin_menu('Stats And Configurations','Configuration CAS','edit_auth_cas.php',AUTH_CAS.'/admin');
+	add_admin_menu('General Configurations','Configuration CAS','edit_auth_cas.php',AUTH_CAS.'/admin');
 
 ?>
