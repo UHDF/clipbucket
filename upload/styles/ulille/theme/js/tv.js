0fb714bc36074b7d776a23cc95d8b0154563f88d
Bastienp <bastien.poirier@univ-lille1.fr>
2017-12-06 16:33:00 +0100

    Patch du script centrant verticalement les modals pour qu'il conserve les inputs radio sélectionnés

--- upload/styles/ulille/theme/js/tv.js
+++ upload/styles/ulille/theme/js/tv.js
@@ -44,7 +44,11 @@ function centerModals($element) {
     $modals = $('.modal-vcenter:visible');
   }
   $modals.each( function(i) {
-    var $clone = $(this).clone().css('display', 'block').appendTo('body');
+    var $clone = $(this).clone().css('display', 'block');
+    $clone.find('input[type="radio"]').each(function(){
+       $(this).attr('name', $(this).attr('name') + '_clone'); 
+    });
+    $clone.appendTo('body');
     var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
     top = top > 0 ? top : 0;
     $clone.remove();
