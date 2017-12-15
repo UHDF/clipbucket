9034d30098023204f55a0b52b10efb1b28ed2672
franck <franck@frk64>
2017-12-15 15:03:13 +0100

    Change ulille frontoffice Template

--- upload/plugins/importCSV/importCSV.php
+++ upload/plugins/importCSV/importCSV.php
@@ -3,7 +3,7 @@
  Plugin Name: importCSV
  Description: Import data from CSV files into clipbucket tables
  Author: Franck Rouze
- Author Website: http://semm.univ-lille1.fr/
+ Author Website: http://www.univ-lille.fr/
  ClipBucket Version: 2.8.1
  Version: 1.0
  Website:
