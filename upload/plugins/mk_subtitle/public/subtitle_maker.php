ed8d6e589b484cbece61c08103d390b5c53d9eb6
Adrien Ponchelet <adrien.ponchelet@u-picardie.fr>
2018-10-26 16:26:20 +0200

    [plugin] apply modification to public area mk_subtitle

--- upload/plugins/mk_subtitle/public/subtitle_maker.php
+++ upload/plugins/mk_subtitle/public/subtitle_maker.php
@@ -161,7 +161,29 @@ else{
 *	Update the marker file
 */
 if ($_POST['saveMarker']){
-	updateFile($marker, $_POST['submarker']);
+
+	$post = array();
+	$orgFile = file($marker);
+	$postedLine = explode("\n", $_POST['submarker']);
+	$data = '';
+
+	foreach ($postedLine as $line){
+		$postedArray = explode("\t", $line);
+		$post[$postedArray[2]] = $postedArray;
+	}
+
+	foreach ($orgFile as $line){
+		$original = explode("\t", $line);
+
+		if (isset($post[$original[2]])) {
+			$data .= $post[$original[2]][0]."\t".$post[$original[2]][1]."\t".$post[$original[2]][2]."\t".$post[$original[2]][3]."\t".$post[$original[2]][4]."\n";
+		}
+		else{
+			$data .= $original[0]."\t".$original[1]."\t".$original[2]."\t".$original[3]."\t".$original[4]."";
+		}
+	}
+
+	updateFile($marker, $data);
 }
 
 /**
